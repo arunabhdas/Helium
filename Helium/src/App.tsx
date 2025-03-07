5c7fab9111e97252b535120d61c7c695112b09ad
Arunabh Das <arunabhdas@gmail.com>
2025-03-07 03:07:11 -0500

    Added dark mode using theme-provider

--- Helium/src/App.tsx
+++ Helium/src/App.tsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className='flex flex-col items-center justify-center min-h-svh'>
         <Button>Click me</Button>
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
