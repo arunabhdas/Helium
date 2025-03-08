7cb3dc30cbc73975381409cf3e3be124583a6897
Arunabh Das <arunabhdas@gmail.com>
2025-03-08 04:21:59 -0500

    Added '@nuxtjs/color-mode' to nuxt.config.ts

--- Helium-nuxt/helium/nuxt.config.ts
+++ Helium-nuxt/helium/nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  modules: [
+    '@nuxtjs/tailwindcss', 
+    '@nuxtjs/color-mode',
+    'shadcn-nuxt'
+  ],
   shadcn: {
      /**
      * Prefix for all the imported component
